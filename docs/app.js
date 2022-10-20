696535b1b623b9d6d65ecdd1723ff76264ea023c
cmachin <carlos.machin@gmail.com>
2022-10-20 19:04:39 -0400

    changed urls

--- docs/app.js
+++ docs/app.js
@@ -6,7 +6,7 @@ function show(option) {
 	// Hide content from other tabs and reset nav bar font color
 	$("h2").css("color", "white");
 	$("main > *").hide();
-	
+
 	// Show the selected tab's content and mark it with a different font color
 	$("#" + option).show();
 	$("#" + option + "Tab").css("color", "#444f61");
