56cec1b0f79d62329614b2774a43c1c080bda694
Carlos Machin <carlos.machin@upr.edu>
2022-10-20 18:06:18 -0400

    formatting

--- docs/app.js
+++ docs/app.js
@@ -4,9 +4,9 @@ function show(option) {
 	console.log("Showing: " + option);
 
 	// Hide content from other tabs and reset nav bar font color
-	$("nav > *").css("color", "#000000");
+	$("h2").css("color", "white");
 	$("main > *").hide();
-
+	
 	// Show the selected tab's content and mark it with a different font color
 	$("#" + option).show();
 	$("#" + option + "Tab").css("color", "#444f61");
