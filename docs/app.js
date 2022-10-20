f7a626f1d363aa98b51cd04dd14a89644ae10e16
cmachin <carlos.machin@gmail.com>
2022-10-20 16:15:08 -0400

    further changes

--- docs/app.js
+++ docs/app.js
@@ -1,16 +1,13 @@
+show("resume");
+
 function show(option) {
 	console.log("Showing: " + option);
 
-	hideAll();
+	// Hide content from other tabs and reset nav bar font color
+	$("nav > *").css("color", "#000000");
+	$("main > *").hide();
 
+	// Show the selected tab's content and mark it with a different font color
 	$("#" + option).show();
-}
-
-function hideAll() {
-	$("#resume").hide();
-	$("#education").hide();
-	$("#work").hide();
-	$("#research").hide();
-	$("#certifications").hide();
-	$("#skills").hide();
+	$("#" + option + "Tab").css("color", "#444f61");
 }
