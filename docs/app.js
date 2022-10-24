09fea889428f279d4bf61d3d2d6a3a747afeae46
Carlos Machin <carlos.machin@upr.edu>
2022-10-23 22:43:42 -0400

    nav animation

--- docs/app.js
+++ docs/app.js
@@ -1,10 +1,12 @@
+nav_open = false;
+
 show("about");
 
 function show(option) {
 	console.log("Showing: " + option);
 
 	// Hide content from other tabs and reset nav bar font color
-	$("h2").css("color", "white");
+	$("#hamburguer nav h3").css("color", "white");
 	$("main > *").hide();
 
 	// Show the selected tab's content and mark it with a different font color
@@ -15,9 +17,14 @@ function show(option) {
 function sidebar() {
 	console.log("Showing sidebar");
 
-	if ($("#hamburguer nav").is(":visible")) {
-		$("#hamburguer nav").hide();
+	if (nav_open) {
+		// $("#hamburguer nav").hide();
+		$("#hamburguer nav").css("left", "-10rem");
 	} else {
-		$("#hamburguer nav").show();
+		// $("#hamburguer nav").show();
+		$("#hamburguer nav").css("left", "0");
+
 	}
+	
+	nav_open = !nav_open;
 }
