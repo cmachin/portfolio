3ef5402be78667d9ea677de390904782cf960d3a
cmachin <carlos.machin@gmail.com>
2023-01-28 19:10:33 -0400

    removed useNavigate

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,29 +1,19 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { useNavigate } from "react-router-dom";
 
 export default function Navigation() {
-	const navigate = useNavigate();
 	return (
 		<Navbar bg="dark" variant="dark" expand="lg" sticky="top" collapseOnSelect>
 			<Container>
-				<Navbar.Brand className="brand-name">Carlos's Portfolio</Navbar.Brand>
+				<Navbar.Brand>Carlos's Portfolio</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse id="responsive-navbar-nav">
+				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link href="#" onClick={() => navigate("/home")}>
-							Home
-						</Nav.Link>
-						<Nav.Link href="#" onClick={() => navigate("/projects")}>
-							Projects
-						</Nav.Link>
-						<Nav.Link href="#" onClick={() => navigate("/work")}>
-							Work
-						</Nav.Link>
-						<Nav.Link href="#" onClick={() => navigate("/research")}>
-							Research
-						</Nav.Link>
+						<Nav.Link href="/#/home">Home</Nav.Link>
+						<Nav.Link href="/#/projects">Projects</Nav.Link>
+						<Nav.Link href="/#/work">Work</Nav.Link>
+						<Nav.Link href="/#/research">Research</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
