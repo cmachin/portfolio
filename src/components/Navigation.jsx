8e7737ca0ff0af5f4f48df1defd2cc79b8db1dd9
cmachin <carlos.machin@gmail.com>
2023-01-28 19:15:35 -0400

    route bug fix

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,10 +10,10 @@ export default function Navigation() {
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
-						<Nav.Link href="/#/home">Home</Nav.Link>
-						<Nav.Link href="/#/projects">Projects</Nav.Link>
-						<Nav.Link href="/#/work">Work</Nav.Link>
-						<Nav.Link href="/#/research">Research</Nav.Link>
+						<Nav.Link href="#/home">Home</Nav.Link>
+						<Nav.Link href="#/projects">Projects</Nav.Link>
+						<Nav.Link href="#/work">Work</Nav.Link>
+						<Nav.Link href="#/research">Research</Nav.Link>
 					</Nav>
 				</Navbar.Collapse>
 			</Container>
