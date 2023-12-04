d6107ab7ec13edc5af7a32d376159701bc24ba52
Carlos <machin.cam@gmail.com>
2023-12-04 17:38:53 -0600

    Updates

---
+++ assets/Certifications-5366d8af.js
@@ -0,0 +1,1 @@
+import{j as i,k as a,C as s}from"./index-20f98fa6.js";import{R as e}from"./Row-d9b09795.js";import{c}from"./database.api-ad3966ef.js";function m(){return i("div",{className:"certifications",children:c().map((r,t)=>a(s,{children:[i("h1",{children:r.title}),i(e,{className:"item-cards",children:i("img",{src:r.image,alt:r.title})})]},t))})}export{m as default};
